ef0ed4dba9ab5cff3b74c790626f7f469c77afd1
cloudyhug <cloudyhug@protonmail.ch>
2019-03-30 14:36:00 +0100

    adding initial dotnet project files

---
+++ Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open System.Collections.Generic
+
+let primes = new List<int>([2; 3])
+
+let isPrime n =
+  if n < 2 || primes.Exists (fun d -> n % d = 0) then false else
+  primes.Add n
+  true
+
+[<EntryPoint>]
+let main argv =
+  printfn "%d\n%d" 2 3
+  let mutable i = 0
+  while i < 15 do
+    let n = 6 * i
+    if isPrime (n - 1) then printfn "%d" (n - 1)
+    if isPrime (n + 1) then printfn "%d" (n + 1)
+    i <- i + 1
+  0
